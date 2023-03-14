00e61f7a48d08a285efcc4ec7b98c688361c8a34
Rajesh Kannan M <rajeshkannan.offl@gmail.com>
2023-03-14 10:24:16 +0530

    Personal Update

---
+++ perl/arithmetics.pl
@@ -0,0 +1,14 @@
+print "Hello World\n";
+print "Arithmetic Operators : User-Input";
+print "\nEnter a: ";
+$a = <STDIN>;		#value of a
+print "Enter b: ";
+$b = <STDIN>;		#value of b
+$c = $a + $b;		#addition
+print "Addition of a & b: $c\n";
+$d = $a - $b;		#subtraction
+print "Subtraction of a & b: $d\n";
+$e = $a / $b;		#division
+print "Division of a & b: $e\n";
+$f = $a % $b;		#modulo
+print "Modulo of a & b: $f\n";
