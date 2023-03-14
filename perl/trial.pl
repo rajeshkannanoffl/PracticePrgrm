63b418e78ccc8293e561e3ff99ac61e18e5298a5
Rajesh Kannan M <rajeshkannan.offl@gmail.com>
2023-03-14 10:14:14 +0530

    Personal Update

---
+++ perl/trial.pl
@@ -0,0 +1,16 @@
+print "Hello World\n";
+print "I'm
+	Here\n";
+
+print "Enter a: ";
+$a = <STDIN>;
+print "Enter b: ";
+$b = <STDIN>;
+$c = $a + $b;
+print "Addition of a & b: $c\n";
+$d = $a - $b;
+print "Subtraction of a & b: $d\n";
+$e = $a / $b;
+print "Division of a & b: $e\n";
+$f = $a % $b;
+print "Modulo of a & b: $f\n";
