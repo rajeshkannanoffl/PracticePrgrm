52909c93279eac5edbd921be49e8d97fb60fc669
Rajesh Kannan M <rajeshkannan.offl@gmail.com>
2023-04-20 10:30:59 +0530

    Personal Update

---
+++ py/hello.py
@@ -0,0 +1,10 @@
+# to print hello world
+print ("1. Hello World")
+
+# print pre-defined 
+new_str = "2. Hello World"
+print (new_str)
+
+# print from user-input
+input_str = input("Enter a word: ")
+print (input_str)
