3aa62bb72766653193d2ecdfc6a8043e1f8d4170
Rajesh Kannan M <rajeshkannan.offl@gmail.com>
2023-04-25 15:18:14 +0530

    Personal Update

--- py/file-palindrome.py
+++ py/file-palindrome.py
@@ -1,7 +1,4 @@
 # function to reverse and check a word
-import time
-from time import perf_counter
-
 def check_palindrome(check_string):
     check_string = check_string.lower()
     reverse_string = ""
@@ -17,10 +14,7 @@ with open("/home/adminrk/works2/py/empty.txt") as file:
 #get_string = input("Enter a string: ")
 split_string = get_string.split(" ")
 final_string = ""
-start = time.perf_counter()
 for i in range(0, len(split_string)):
     if check_palindrome(str(split_string[i])):
         final_string = final_string + split_string[i] + "\n"
-end = time.perf_counter()
-print(end)
 print (final_string)
